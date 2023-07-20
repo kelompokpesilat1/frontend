e75072910f2915006748a1b2fdc8eacb333c86ac
unknown <zulfikarmuhamad432@gmail.com>
2023-07-20 12:41:26 +0700

    feat: (+)page kategori, (+)pasang material icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,10 @@ export default {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat+Alternates:wght@700&family=Montserrat:wght@300;400;500;600;700&display=swap',
       },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+      },
     ],
 
     //     <link rel="preconnect" href="https://fonts.googleapis.com">
--- pages/artikel-terbaru.vue
+++ pages/artikel-terbaru.vue
@@ -3,36 +3,22 @@ import { dummyArtikel } from '../utils/dummyData'
 </script>
 
 <template>
-  <div class="container mx-auto my-10">
-    <section class="flex items-center justify-between h-[75px]">
-      <nuxt-link to="/artikel-terbaru"
-        ><button class="text-red-600">Kembali</button></nuxt-link
-      >
-    </section>
-    <section>
-      <div
-        class="artikel-container grid md:grid-cols-2 lg:grid-cols-4 gap-5 my-5"
-      >
-        <ArtikelCard
-          v-for="artikel in dummyArtikel"
-          :key="artikel.id"
-          a
-          :artikel="artikel"
-        />
-        <ArtikelCard
-          v-for="artikel in dummyArtikel"
-          :key="artikel.id"
-          a
-          :artikel="artikel"
-        />
-        <ArtikelCard
-          v-for="artikel in dummyArtikel"
-          :key="artikel.id"
-          a
-          :artikel="artikel"
-        />
-      </div>
-    </section>
+  <div>
+    <Navigation />
+    <Container>
+      <section>
+        <div
+          class="artikel-container grid md:grid-cols-2 lg:grid-cols-4 gap-5 my-5"
+        >
+          <ArtikelCard
+            v-for="artikel in dummyArtikel"
+            :key="artikel.id"
+            a
+            :artikel="artikel"
+          />
+        </div>
+      </section>
+    </Container>
   </div>
 </template>
 
