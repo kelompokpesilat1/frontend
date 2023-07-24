5db75cead3f8d7bb0eed979edf38806589f31583
WINDOWS-2OGN1V3\User <halditsa@gmail.com>
2023-07-24 13:25:46 +0700

    test

---
+++ components/Modal/UpdateUsers.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div class="bg-slate-200 rounded-lg shadow-lg p-6 w-96">
+      <h2 class="text-xl font-semibold mb-4">Edit Form</h2>
+      <div>
+        <label class="block text-gray-700 font-semibold mb-2">Name</label>
+        <input type="text" placeholder="title" @change="event => handleInputChange(event, 'title')" v-model="inputValue.nama" class="w-full"/>
+      <div class="mt-4">
+        <label class="block text-gray-700 font-semibold mb-2">Email</label>
+        <input type="text" placeholder="kategori" @change="event => handleInputChange(event, 'kategori')" v-model="inputValue.email"/>
+        <label class="block text-gray-700 font-semibold mb-2">Roles</label>
+        <select class="p-1 border rounded" v-model="role.name">
+              <option v-for="option in roleOptions" :key="option.value" :value="option.value">{{ option.label }}</option>
+            </select>
+        <input type="text" placeholder="Penulis" @change="event => handleInputChange(event, 'author')" v-model="inputValue.author"/>
+        <label class="block text-gray-700 font-semibold mb-2">Tanggal Publish</label>
+        <input type="text" placeholder="27 juni 2022" @change="event => handleInputChange(event, 'date')" v-model="inputValue.date"/>
+      <div class="mt-6 flex justify-end">
+        <button @click="handleClose" class="px-4 py-2 mr-2 rounded text-white bg-gray-500">Cancel</button>
+        <button @click="handleSave" class="px-4 py-2 rounded text-white bg-blue-500">Save</button>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    usersEdit: {
+      type: Object,
+      required: true,
+    },
+    handleInputChange : {
+      type: Function,
+      required: true,
+    },
+    inputValue : {
+      type: Object,
+      required: true,
+    },
+    handleClose: {
+      type: Function,
+      required: true,
+    },
+    handleSave : {
+      type: Function,
+      required: true,
+    }
+  }
+}
+  
+</script>
